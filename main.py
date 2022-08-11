66ff4dc1d16d79abf973d150582baf4ef706360f
SomeRandomOnlineDev <alansherwood08@gmail.com>
2022-08-11 10:10:58 +0800

    We are updating to v1.2.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def clear_result():
     for w in result.winfo_children():
@@ -13,7 +14,7 @@ def binary_to_text(convert):
 
     # the line below is copy and pasted, just to let you know, I'm probably not as good as python as you think I am
     ascii_string = "".join([chr(int(binary, 2)) for binary in convert.split(" ")])
-    tk.Label(result, text=ascii_string).pack()
+    ttk.Label(result, text=ascii_string).pack()
 
     result.grid(row=2, column=0, padx=3, pady=3)
 
@@ -25,7 +26,7 @@ def text_to_binary(convert):
     for x in convert:  # iterates over each character in the list
         ascii_values = ord(x)  # gets the ascii value for x
         binary = format(ascii_values, '08b')
-        tk.Label(result, text=f'{x} = {binary}').pack()
+        ttk.Label(result, text=f'{x} = {binary}').pack()
 
     result.grid(row=2, column=0, padx=3, pady=3)
 
@@ -43,25 +44,27 @@ def mode_execution():
 window = tk.Tk()
 window.title('Binary Converter')
 
-entry = tk.Entry(window, width=20)
+window.minsize(330, 80)
+
+entry = ttk.Entry(window, width=20)
 entry.grid(row=0, column=0, padx=3, pady=3)
 
-options = ['Text to binary', 'Binary to text']
+options = ['Select a conversion mode', 'Text to binary', 'Binary to text']
 
 variable = tk.StringVar()
 variable.set(options[0])
 
 # frame for the conversion result
-result = tk.Frame(master=window)
+result = ttk.Frame(master=window)
 result.grid(row=2, column=0, padx=3, pady=3)
 
-drop = tk.OptionMenu(window, variable, *options)
+drop = ttk.OptionMenu(window, variable, *options)
 drop.grid(row=0, column=1, padx=3, pady=3)
 
-btn_clear_result = tk.Button(master=window, relief=tk.RIDGE, text='Clear results', command=lambda: clear_result())
+btn_clear_result = ttk.Button(master=window, text='Clear results', command=lambda: clear_result())
 btn_clear_result.grid(row=1, column=1, padx=3, pady=3)
 
-btn_misc_convert = tk.Button(master=window, relief=tk.RIDGE, text='Convert it!', command=lambda: mode_execution())
+btn_misc_convert = ttk.Button(master=window, text='Convert it!', command=lambda: mode_execution())
 btn_misc_convert.grid(row=1, column=0, padx=3, pady=3)
 
 # these are relics from a previous version, still here, just in case
